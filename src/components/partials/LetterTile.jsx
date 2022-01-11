f30a2053154c86b8129a7b16982c7b0fdf8e10ee
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-11 02:52:38 +0000

    letter tile colouring

--- src/components/partials/LetterTile.jsx
+++ src/components/partials/LetterTile.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { flip, set, useSpring, animated } from 'react-spring'
+
 
 function LetterTile(props) {
+
+
+
+    const baseClass = "flex h-15 w-15 spacing-15 gap-1 justify-center items-center text-center text-4xl font-bold  "
+    const [classStyling, setClassStyling] = useState(baseClass + "border border-neutral-600")
+
+
+
+    function updateStyling(status) {
+        if (status === "incorrect") {
+            setClassStyling(baseClass + "bg-neutral-600 tile-flip")
+
+        }
+
+        if (status === "correct") {
+            setClassStyling(baseClass + "bg-tile-correct-green tile-flip")
+        }
+
+        if (status === "exists") {
+            setClassStyling(baseClass + "bg-tile-exists-yellow tile-flip")
+        }
+    }
+
+
+    useEffect(() => {
+        updateStyling(props.status)
+    }, [props.status])
+
+
+
+
     return (
-        <div className="p-2  gap-1 border border-neutral-600 text-center  text-3xl font-semibold">
-            <span>{props.children || <>&nbsp;</>}</span>
+
+        <div className={classStyling}>
+            <span className="letter">{props.children || <>&nbsp;</>}</span>
         </div>
+
+
     )
 }
 
