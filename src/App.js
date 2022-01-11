24f0987a12c78d7617f5bcf25da9f1f2eebedabc
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-11 02:50:41 +0000

    overflow

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import GameScreen from "./components/GameScreen";
 
 function App() {
   return (
-    <div className="App mt-2 mx-auto justify-center max-w-md min-h-screen">
+    <div className="App mx-auto justify-center max-w-md h-screen overflow-hidden">
       <GameScreen />
     </div>
   );
