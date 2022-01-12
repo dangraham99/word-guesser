c878c9bf7652cbe46ee4520a4469ce77b0197cf1
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-12 00:27:14 +0000

    extra props for animation + check if live tile

--- src/components/partials/BoardRow.jsx
+++ src/components/partials/BoardRow.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import LetterTile from './LetterTile'
 
 
 function BoardRow(props) {
 
-    const [activeRow, setActiveRow] = useState(false)
 
+    const [reveal, setReveal] = useState(false)
 
+    useEffect(() => {
+        if (props.currentPos.guess > props.rowNumber) {
+            setReveal(true)
+        }
+
+    }, [props.currentPos.guess])
 
     return (
 
         <div className="flex justify-center min-w-full space-x-1">
             {
                 props.guess.map((letterObject, index) => {
-                    return <LetterTile status={letterObject.status} key={index}>{letterObject.val}</LetterTile>
+                    return <LetterTile activeTile={props.activeRow && props.currentPos.character === index} reveal={reveal} status={letterObject.status} timeOutDelay={index + 1} key={index}>{letterObject.val}</LetterTile>
                 })
             }
         </div>
