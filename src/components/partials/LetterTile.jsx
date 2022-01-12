cc21146895867ab31e078ec333af355c81aa62be
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-12 00:24:14 +0000

    animations

--- src/components/partials/LetterTile.jsx
+++ src/components/partials/LetterTile.jsx
@@ -1,44 +1,78 @@
 import React, { useState, useEffect } from 'react'
-import { flip, set, useSpring, animated } from 'react-spring'
+import { CSSTransition } from 'react-transition-group'
 
 
 function LetterTile(props) {
 
 
 
-    const baseClass = "flex h-15 w-15 spacing-15 gap-1 justify-center items-center text-center text-4xl font-bold  "
+    const baseClass = "flex h-15 w-15 spacing-15 gap-1 justify-center items-center text-center text-4xl font-bold "
     const [classStyling, setClassStyling] = useState(baseClass + "border border-neutral-600")
 
 
 
-    function updateStyling(status) {
-        if (status === "incorrect") {
-            setClassStyling(baseClass + "bg-neutral-600 tile-flip")
+
+
+
+    function updateStyling() {
+        if (props.status === "incorrect") {
+            setClassStyling(baseClass + "bg-neutral-600")
 
         }
 
-        if (status === "correct") {
-            setClassStyling(baseClass + "bg-tile-correct-green tile-flip")
+        if (props.status === "correct") {
+            setClassStyling(baseClass + "bg-tile-correct-green")
         }
 
-        if (status === "exists") {
-            setClassStyling(baseClass + "bg-tile-exists-yellow tile-flip")
+        if (props.status === "exists") {
+            setClassStyling(baseClass + "bg-tile-exists-yellow")
         }
+
+
     }
 
 
     useEffect(() => {
-        updateStyling(props.status)
-    }, [props.status])
+
+        if (props.reveal) {
+            updateStyling()
+        }
+
+
+
+    }, [props.reveal])
+
+
+    useEffect(() => {
+
+        if (props.children) {
+            setClassStyling(classStyling + " letter-pop")
+        }
+
+        if (!props.children) {
+            setClassStyling(classStyling.replace("letter-pop", ""))
+        }
+
+    }, [props.children])
+
+
+
+
+
+
+
 
 
 
 
     return (
 
-        <div className={classStyling}>
-            <span className="letter">{props.children || <>&nbsp;</>}</span>
-        </div>
+        <CSSTransition in={props.reveal} classNames={"tile-flip"} appear={true} timeout={300 * props.timeOutDelay}>
+            <div className={classStyling}>
+                <span className="letter">{props.children || <>&nbsp;</>}</span>
+            </div>
+
+        </CSSTransition>
 
 
     )
