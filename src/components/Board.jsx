d8878fd817c431af534d8d559463cf8245b6294a
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-12 23:54:52 +0000

    shake anim

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,14 +4,16 @@ import BoardRow from './partials/BoardRow'
 function Board(props) {
     return (
 
+
         <div id="board" className="grid grid-rows-6 gap-1 select-none">
-            <BoardRow rowNumber={0} currentPos={props.currentPos} activeRow={props.currentPos.guess === 0} guess={props.layout[0]} />
-            <BoardRow rowNumber={1} currentPos={props.currentPos} activeRow={props.currentPos.guess === 1} guess={props.layout[1]} />
-            <BoardRow rowNumber={2} currentPos={props.currentPos} activeRow={props.currentPos.guess === 2} guess={props.layout[2]} />
-            <BoardRow rowNumber={3} currentPos={props.currentPos} activeRow={props.currentPos.guess === 3} guess={props.layout[3]} />
-            <BoardRow rowNumber={4} currentPos={props.currentPos} activeRow={props.currentPos.guess === 4} guess={props.layout[4]} />
-            <BoardRow rowNumber={5} currentPos={props.currentPos} activeRow={props.currentPos.guess === 5} guess={props.layout[5]} />
+            <BoardRow invalidGuess={props.invalidGuess} rowNumber={0} currentPos={props.currentPos} activeRow={props.currentPos.guess === 0} guess={props.layout[0]} />
+            <BoardRow invalidGuess={props.invalidGuess} rowNumber={1} currentPos={props.currentPos} activeRow={props.currentPos.guess === 1} guess={props.layout[1]} />
+            <BoardRow invalidGuess={props.invalidGuess} rowNumber={2} currentPos={props.currentPos} activeRow={props.currentPos.guess === 2} guess={props.layout[2]} />
+            <BoardRow invalidGuess={props.invalidGuess} rowNumber={3} currentPos={props.currentPos} activeRow={props.currentPos.guess === 3} guess={props.layout[3]} />
+            <BoardRow invalidGuess={props.invalidGuess} rowNumber={4} currentPos={props.currentPos} activeRow={props.currentPos.guess === 4} guess={props.layout[4]} />
+            <BoardRow invalidGuess={props.invalidGuess} rowNumber={5} currentPos={props.currentPos} activeRow={props.currentPos.guess === 5} guess={props.layout[5]} />
         </div>
+
 
 
     )
--- src/components/partials/BoardRow.jsx
+++ src/components/partials/BoardRow.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import LetterTile from './LetterTile'
+import 'animate.css';
 
 
 function BoardRow(props) {
 
 
     const [reveal, setReveal] = useState(false)
+    const [classStyling, setClassStyling] = useState("flex justify-center min-w-full space-x-1")
 
     useEffect(() => {
         if (props.currentPos.guess > props.rowNumber) {
@@ -14,15 +16,35 @@ function BoardRow(props) {
 
     }, [props.currentPos.guess])
 
+
+    useEffect(() => {
+
+        if (props.invalidGuess && props.activeRow) {
+
+            setClassStyling(classStyling + " animate__animated animate__shakeX")
+
+
+        }
+
+    }, [props.invalidGuess])
+
+
+
+
+
+
     return (
 
-        <div className="flex justify-center min-w-full space-x-1">
+        <div className={classStyling} onAnimationEnd={() => { setClassStyling(classStyling.replace(" animate__animated animate__shakeX", "")) }}>
             {
+
                 props.guess.map((letterObject, index) => {
                     return <LetterTile activeTile={props.activeRow && props.currentPos.character === index} reveal={reveal} status={letterObject.status} timeOutDelay={index + 1} key={index}>{letterObject.val}</LetterTile>
                 })
+
             }
         </div>
+
 
 
     )
