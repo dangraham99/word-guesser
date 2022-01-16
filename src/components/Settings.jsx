7c9fa284c43e83917e37ceb48c722c6ccae7785a
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-16 04:00:24 +0000

    dark locale button styling

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -32,11 +32,11 @@ function Settings() {
                                         <p className="text-sm text-neutral-500">Changes the language and available keyboards</p>
                                     </div>
                                     <div class="inline-flex rounded-md shadow-sm" role="group">
-                                        <button type="button" class="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                        <button type="button" class="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-neutral-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-neutral-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
                                             English
                                         </button>
 
-                                        <button type="button" class="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                                        <button type="button" class="py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-neutral-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-neutral-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
                                             Deutsch
                                         </button>
                                     </div>
