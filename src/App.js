8ac7a35284b7e76decb40a291fad31c24ac84cc2
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-16 03:52:58 +0000

    React router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import GameScreen from "./components/GameScreen";
+import Settings from "./components/Settings"
+import About from "./components/About"
 
 function App() {
   return (
     <div className="App mx-auto justify-center max-w-md h-screen overflow-hidden">
-      <GameScreen />
+      <Router>
+        <Routes>
+          <Route index element={<GameScreen />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
